d0178d08e23a35060a456d7d5711276929dbc6a1
basel8 <basel84mn@gmail.com>
2019-04-16 19:24:14 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,116 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React, { useContext, useReducer, useState } from "react";
+import ReactDOM from "react-dom";
+import uuidv1 from "uuid/v1";
+function reducer(state, action) {
+  switch (action.type) {
+    case "addTodo":
+      const todo = action.todo;
+      return { ...state, todos: [...state.todos, todo] };
+    case "toggleTodo":
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id === action.id) {
+            todo.complete = !todo.complete;
+          }
+          return todo;
+        })
+      };
+    case "deleteTodo":
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id)
+      };
+    case "editTodo":
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id === action.id) {
+            todo.text = action.text;
+          }
+          return todo;
+        })
+      };
+    default:
+      return state;
+  }
+}
+const TodosContext = React.createContext({
+  todos: [
+    {
+      id: 0,
+      text: "Hooks Todo List :)",
+      complete: true
+    }
+  ]
+});
+function App() {
+  const initialState = useContext(TodosContext);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div className="App">
+      <TodosContext.Provider value={{ state, dispatch }}>
+        <TodosList />
+      </TodosContext.Provider>
+    </div>
+  );
+}
+function TodosList() {
+  const { state, dispatch } = useContext(TodosContext);
+  const [value, setValue] = useState("");
+  const [{ editMode, id }, setEditMode] = useState({
+    editMode: false,
+    id: ""
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (editMode) {
+      dispatch({
+        type: "editTodo",
+        id,
+        text: value
+      });
+      setEditMode({ editMode: false, id: "" });
+    } else {
+      dispatch({
+        type: "addTodo",
+        todo: { id: uuidv1(), text: value, complete: false }
+      });
+    }
+    setValue("");
+  };
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  const handleToggle = (id) => {
+    dispatch({ type: "toggleTodo", id });
+  };
+  const handleDelete = (id) => {
+    dispatch({ type: "deleteTodo", id });
+  };
+  const handleEdit = (id, text) => {
+    setEditMode({ editMode: true, id });
+    setValue(text);
+  };
+  return (
+    <div>
+      <h3>Hooks Todo List</h3>
+      {state.todos.map((todo) => (
+        <li
+          key={todo.id}
+          style={{ textDecoration: todo.complete ? "line-through" : "none" }}
+          onDoubleClick={() => handleToggle(todo.id)}
+        >
+          {todo.text}
+          <button onClick={() => handleDelete(todo.id)}>delete</button>
+          <button onClick={() => handleEdit(todo.id, todo.text)}>Edit</button>
+        </li>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} value={value} placeholder="Add Todos" />
+      </form>
+    </div>
+  );
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
